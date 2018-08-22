1c54681bffbde829d74fb95d7ba1c8c9a9d82466
Mike Simms <msimms01@gmail.com>
2018-08-22 14:50:47 -0400

    Cleaned up the C++ test program.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -23,44 +23,68 @@
 #include "IsolationForest.h"
 #include <stdlib.h>
 #include <inttypes.h>
+#include <iostream>
+#include <fstream>
 
 using namespace IsolationForest;
 
 int main(int argc, const char * argv[])
 {
-	Forest forest(10, 10);
+	const size_t NUM_TRAINING_SAMPLES = 100;
+	const size_t NUM_TEST_SAMPLES = 10;
+	const size_t NUM_TREES_IN_FOREST = 10;
+	const size_t SUBSAMPLING_SIZE = 10;
+
+	std::ofstream outStream;
+
+	// Parse the command line arguments.
+	for (int i = 1; i < argc; ++i)
+	{
+		if ((strstr(argv[i], "outfile") == 0) && (i + 1 < argc))
+		{
+			outStream.open(argv[i + 1]);
+		}
+	}
+
+	Forest forest(NUM_TREES_IN_FOREST, SUBSAMPLING_SIZE);
 
 	srand((unsigned int)time(NULL));
 
-	// Training samples.
-	for (size_t i = 0; i < 100; ++i)
+	// Create some training samples.
+	for (size_t i = 0; i < NUM_TRAINING_SAMPLES; ++i)
 	{
-		Sample sample("");
+		Sample sample("training");
 		FeaturePtrList features;
 
-		uint32_t x = 0.3 * (rand() % 100);
-		uint32_t y = 0.3 * (rand() % 100);
+		uint32_t x = rand() % 25;
+		uint32_t y = rand() % 25;
 
 		features.push_back(new Feature("x", x));
 		features.push_back(new Feature("y", y));
 
 		sample.AddFeatures(features);
 		forest.AddSample(sample);
+
+		if (outStream.is_open())
+		{
+			outStream << "training," << x << "," << y << std::endl;
+		}
 	}
 
 	// Create the isolation forest.
 	forest.Create();
 
 	// Test samples (similar to training samples).
-	printf("Test samples that are similar to the training set.\n");
-	printf("--------------------------------------------------\n");
-	for (size_t i = 0; i < 10; ++i)
+	std::cout << "Test samples that are similar to the training set." << std::endl;
+	std::cout << "--------------------------------------------------" << std::endl;
+	double avgNormalScore = (double)0.0;
+	for (size_t i = 0; i < NUM_TEST_SAMPLES; ++i)
 	{
-		Sample sample("");
+		Sample sample("normal sample");
 		FeaturePtrList features;
 
-		uint32_t x = 0.3 * (rand() % 100);
-		uint32_t y = 0.3 * (rand() % 100);
+		uint32_t x = rand() % 25;
+		uint32_t y = rand() % 25;
 
 		features.push_back(new Feature("x", x));
 		features.push_back(new Feature("y", y));
@@ -68,20 +92,28 @@ int main(int argc, const char * argv[])
 
 		// Run a test with the sample that doesn't contain outliers.
 		double score = forest.Score(sample);
-		printf("Normal test sample %" PRIuPTR ": %lf\n", i, score);
+		std::cout << "Normal test sample " << i << ": " << score << std::endl;
+
+		if (outStream.is_open())
+		{
+			outStream << "normal," << x << "," << y << std::endl;
+		}
+
+		avgNormalScore += score;
 	}
-	printf("\n");
+	std::cout << std::endl;
 
 	// Outlier samples (different from training samples).
-	printf("Test samples that are different from the training set.\n");
-	printf("------------------------------------------------------\n");
-	for (size_t i = 0; i < 10; ++i)
+	std::cout << "Test samples that are different from the training set." << std::endl;
+	std::cout << "------------------------------------------------------" << std::endl;
+	double avgOutlierScore = (double)0.0;
+	for (size_t i = 0; i < NUM_TEST_SAMPLES; ++i)
 	{
-		Sample sample("");
+		Sample sample("outlier sample");
 		FeaturePtrList features;
 
-		uint32_t x = 25.0 + (0.5 * (rand() % 50));
-		uint32_t y = 25.0 + (0.5 * (rand() % 50));
+		uint32_t x = 20 + (rand() % 25);
+		uint32_t y = 20 + (rand() % 25);
 
 		features.push_back(new Feature("x", x));
 		features.push_back(new Feature("y", y));
@@ -89,9 +121,21 @@ int main(int argc, const char * argv[])
 
 		// Run a test with the sample that contains outliers.
 		double score = forest.Score(sample);
-		printf("Outlier test sample %" PRIuPTR ": %lf\n", i, score);
+		std::cout << "Outlier test sample " << i << ": " << score << std::endl;
+
+		if (outStream.is_open())
+		{
+			outStream << "outlier," << x << "," << y << std::endl;
+		}
+
+		avgOutlierScore += score;
 	}
-	printf("\n");
+	std::cout << std::endl;
+
+	if (outStream.is_open())
+	{
+		outStream.close();
+	}
 
 	return 0;
 }
