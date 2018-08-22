deb8377c5087ec647dca6bb959182cdeb48fea07
Mike Simms <msimms01@gmail.com>
2018-08-22 00:06:38 -0400

    Backup commit of the test script.

--- python/test.py
+++ python/test.py
@@ -22,43 +22,98 @@
 
 import random
 import IsolationForest
+import plotly.plotly as py
+import plotly.graph_objs as go
 
-forest = IsolationForest.Forest(10, 10)
+def main():
+    num_trees = 10
+    sub_sampling_size = 10
+    forest = IsolationForest.Forest(num_trees, sub_sampling_size)
+    num_training_samples = 100
+    num_tests = 10
 
-# Create some training samples.
-for i in range(0,100):
-    sample = IsolationForest.Sample("")
-    features = []
+    # Create some training samples.
+    training_x = []
+    training_y = []
+    for i in range(0,num_training_samples):
+        sample = IsolationForest.Sample("training")
+        features = []
 
-    x = 0.3 * (random.randint(0,100))
-    y = 0.3 * (random.randint(0,100))
+        x = 0.3 * (random.randint(0,100))
+        y = 0.3 * (random.randint(0,100))
 
-    features.append({"x": x})
-    features.append({"y": y})
+        features.append({"x": x})
+        features.append({"y": y})
+        sample.add_features(features)
+        forest.add_sample(sample)
 
-    sample.add_features(features)
-    forest.add_sample(sample)
+        # So we can graph this later.
+        training_x.append(x)
+        training_y.append(y)
 
-# Create the isolation forest.
-forest.create()
+    # Create the isolation forest.
+    forest.create()
 
-# Test samples (similar to training samples).
-print "Test samples that are similar to the training set."
-print "--------------------------------------------------"
-for i in range(0,10):
-    sample = IsolationForest.Sample("")
-    features = []
+    # Test samples (similar to training samples).
+    print "Test samples that are similar to the training set."
+    print "--------------------------------------------------"
+    normal_x = []
+    normal_y = []
+    avg_normal_score = 0.0
+    for i in range(0,num_tests):
+        sample = IsolationForest.Sample("normal sample")
+        features = []
 
-    x = 0.3 * (random.randint(0,100))
-    y = 0.3 * (random.randint(0,100))
+        x = 0.3 * (random.randint(0,100))
+        y = 0.3 * (random.randint(0,100))
 
-    features.append({"x": x})
-    features.append({"y": y})
+        features.append({"x": x})
+        features.append({"y": y})
+        sample.add_features(features)
 
-    sample.add_features(features)
+        # So we can graph this later.
+        normal_x.append(x)
+        normal_y.append(y)
 
-    # Run a test with the sample that doesn't contain outliers.
-    score = forest.score(sample)
-    print "Normal test sample " + str(i) + ": " + str(score)
+        # Run a test with the sample that doesn't contain outliers.
+        score = forest.score(sample)
+        avg_normal_score = avg_normal_score + score
+        print "Normal test sample " + str(i) + ": " + str(score)
+    avg_normal_score = avg_normal_score / num_tests
 
-print "\n"
+    # Test samples (similar to training samples).
+    print "Test samples that are different to the training set."
+    print "----------------------------------------------------"
+    outlier_x = []
+    outlier_y = []
+    avg_outlier_score = 0.0
+    for i in range(0,num_tests):
+        sample = IsolationForest.Sample("outlier sample")
+        features = []
+
+        x = 0.3 * (random.randint(0,100))
+        y = 0.3 * (random.randint(0,100))
+        sample.add_features(features)
+
+        features.append({"x": x})
+        features.append({"y": y})
+
+        # So we can graph this later.
+        outlier_x.append(x)
+        outlier_y.append(y)
+
+        # Run a test with the sample that doesn't contain outliers.
+        score = forest.score(sample)
+        avg_outlier_score = avg_outlier_score + score
+        print "Outlier test sample " + str(i) + ": " + str(score)
+    avg_outlier_score = avg_outlier_score / num_tests
+
+    # Create a trace.
+    training_trace = go.Scatter(x = training_x, y = training_y, mode = 'markers')
+    normal_trace = go.Scatter(x = normal_x, y = normal_y, mode = 'markers')
+    outlier_trace = go.Scatter(x = outlier_x, y = outlier_y, mode = 'markers')
+    data = [training_trace, normal_trace, outlier_trace]
+    py.iplot(data, filename='basic-scatter')
+
+if __name__ == "__main__":
+    main()
