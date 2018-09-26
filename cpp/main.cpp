267ae5b80236e4b9504e83337d1b315fc92fafe3
Mike Simms <msimms01@gmail.com>
2018-09-25 22:35:41 -0400

    Added performance measurements to the C++ implementation.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -25,15 +25,102 @@
 #include <inttypes.h>
 #include <iostream>
 #include <fstream>
+#include <chrono>
 
 using namespace IsolationForest;
+
+void test(std::ofstream& outStream, size_t numTrainingSamples, size_t numTestSamples, uint32_t numTrees, uint32_t subSamplingSize)
+{
+	Forest forest(numTrees, subSamplingSize);
+	
+	std::chrono::high_resolution_clock::time_point startTime = std::chrono::high_resolution_clock::now();
+	
+	// Create some training samples.
+	for (size_t i = 0; i < numTrainingSamples; ++i)
+	{
+		Sample sample("training");
+		FeaturePtrList features;
+		
+		uint32_t x = rand() % 25;
+		uint32_t y = rand() % 25;
+		
+		features.push_back(new Feature("x", x));
+		features.push_back(new Feature("y", y));
+		
+		sample.AddFeatures(features);
+		forest.AddSample(sample);
+		
+		if (outStream.is_open())
+		{
+			outStream << "training," << x << "," << y << std::endl;
+		}
+	}
+	
+	// Create the isolation forest.
+	forest.Create();
+	
+	// Test samples (similar to training samples).
+	double avgNormalScore = (double)0.0;
+	for (size_t i = 0; i < numTestSamples; ++i)
+	{
+		Sample sample("normal sample");
+		FeaturePtrList features;
+		
+		uint32_t x = rand() % 25;
+		uint32_t y = rand() % 25;
+		
+		features.push_back(new Feature("x", x));
+		features.push_back(new Feature("y", y));
+		sample.AddFeatures(features);
+		
+		// Run a test with the sample that doesn't contain outliers.
+		double score = forest.Score(sample);
+		avgNormalScore += score;
+		
+		if (outStream.is_open())
+		{
+			outStream << "normal," << x << "," << y << std::endl;
+		}
+	}
+	
+	// Outlier samples (different from training samples).
+	double avgOutlierScore = (double)0.0;
+	for (size_t i = 0; i < numTestSamples; ++i)
+	{
+		Sample sample("outlier sample");
+		FeaturePtrList features;
+		
+		uint32_t x = 20 + (rand() % 25);
+		uint32_t y = 20 + (rand() % 25);
+		
+		features.push_back(new Feature("x", x));
+		features.push_back(new Feature("y", y));
+		sample.AddFeatures(features);
+		
+		// Run a test with the sample that contains outliers.
+		double score = forest.Score(sample);
+		avgOutlierScore += score;
+		
+		if (outStream.is_open())
+		{
+			outStream << "outlier," << x << "," << y << std::endl;
+		}
+	}
+	
+	std::chrono::high_resolution_clock::time_point endTime = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsedTime = std::chrono::duration_cast<std::chrono::duration<double>>(endTime - startTime);
+
+	std::cout << "Average of normal test samples: " << avgNormalScore << std::endl;
+	std::cout << "Average of outlier test samples: " << avgOutlierScore << std::endl;
+	std::cout << "Total time for Test 1: " << elapsedTime.count() << " seconds." << std::endl;
+}
 
 int main(int argc, const char * argv[])
 {
 	const size_t NUM_TRAINING_SAMPLES = 100;
 	const size_t NUM_TEST_SAMPLES = 10;
-	const size_t NUM_TREES_IN_FOREST = 10;
-	const size_t SUBSAMPLING_SIZE = 10;
+	const uint32_t NUM_TREES_IN_FOREST = 10;
+	const uint32_t SUBSAMPLING_SIZE = 10;
 
 	std::ofstream outStream;
 
@@ -46,90 +133,15 @@ int main(int argc, const char * argv[])
 		}
 	}
 
-	Forest forest(NUM_TREES_IN_FOREST, SUBSAMPLING_SIZE);
-
 	srand((unsigned int)time(NULL));
 
-	// Create some training samples.
-	for (size_t i = 0; i < NUM_TRAINING_SAMPLES; ++i)
-	{
-		Sample sample("training");
-		FeaturePtrList features;
-
-		uint32_t x = rand() % 25;
-		uint32_t y = rand() % 25;
-
-		features.push_back(new Feature("x", x));
-		features.push_back(new Feature("y", y));
-
-		sample.AddFeatures(features);
-		forest.AddSample(sample);
-
-		if (outStream.is_open())
-		{
-			outStream << "training," << x << "," << y << std::endl;
-		}
-	}
-
-	// Create the isolation forest.
-	forest.Create();
-
-	// Test samples (similar to training samples).
-	std::cout << "Test samples that are similar to the training set." << std::endl;
-	std::cout << "--------------------------------------------------" << std::endl;
-	double avgNormalScore = (double)0.0;
-	for (size_t i = 0; i < NUM_TEST_SAMPLES; ++i)
-	{
-		Sample sample("normal sample");
-		FeaturePtrList features;
-
-		uint32_t x = rand() % 25;
-		uint32_t y = rand() % 25;
-
-		features.push_back(new Feature("x", x));
-		features.push_back(new Feature("y", y));
-		sample.AddFeatures(features);
-
-		// Run a test with the sample that doesn't contain outliers.
-		double score = forest.Score(sample);
-		std::cout << "Normal test sample " << i << ": " << score << std::endl;
-
-		if (outStream.is_open())
-		{
-			outStream << "normal," << x << "," << y << std::endl;
-		}
-
-		avgNormalScore += score;
-	}
+	std::cout << "Test 1:" << std::endl;
+	std::cout << "-------" << std::endl;
+	test(outStream, NUM_TRAINING_SAMPLES, NUM_TEST_SAMPLES, NUM_TREES_IN_FOREST, SUBSAMPLING_SIZE);
 	std::cout << std::endl;
-
-	// Outlier samples (different from training samples).
-	std::cout << "Test samples that are different from the training set." << std::endl;
-	std::cout << "------------------------------------------------------" << std::endl;
-	double avgOutlierScore = (double)0.0;
-	for (size_t i = 0; i < NUM_TEST_SAMPLES; ++i)
-	{
-		Sample sample("outlier sample");
-		FeaturePtrList features;
-
-		uint32_t x = 20 + (rand() % 25);
-		uint32_t y = 20 + (rand() % 25);
-
-		features.push_back(new Feature("x", x));
-		features.push_back(new Feature("y", y));
-		sample.AddFeatures(features);
-
-		// Run a test with the sample that contains outliers.
-		double score = forest.Score(sample);
-		std::cout << "Outlier test sample " << i << ": " << score << std::endl;
-
-		if (outStream.is_open())
-		{
-			outStream << "outlier," << x << "," << y << std::endl;
-		}
-
-		avgOutlierScore += score;
-	}
+	std::cout << "Test 2:" << std::endl;
+	std::cout << "-------" << std::endl;
+	test(outStream, NUM_TRAINING_SAMPLES * 10, NUM_TEST_SAMPLES * 10, NUM_TREES_IN_FOREST * 10, SUBSAMPLING_SIZE * 10);
 	std::cout << std::endl;
 
 	if (outStream.is_open())
